0c0d2aef5fd04c7bdc6780d3baab790e46b5bfda
oustella <oustella@gmail.com>
2022-02-28 00:44:10 -0500

    assignment1

---
+++ Flix/MovieCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionViewCell.swift
+//  Flix
+//
+//  Created by OSL on 2/27/22.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Flix/MovieGridViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridViewCell.swift
+//  Flix
+//
+//  Created by OSL on 2/27/22.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+}
